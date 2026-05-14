aaf8afa3a0b4b0c49accb40494e9715f7fa3d261
Michael Herrmann <mh@meisterbetriebherrmann.onmicrosoft.com>
2026-05-14 11:57:47 +0200

    fix: degToRad in snap.ts

--- lib/snap.ts
+++ lib/snap.ts
@@ -70,6 +70,10 @@ export function gridIndicesFromWorldHit(
   return { ix, iz };
 }
 
+export function degToRad(d: number): number {
+  return (d * Math.PI) / 180;
+}
+
 export function radToDeg(r: number): number {
   return (r * 180) / Math.PI;
 }
